ca83e3303f4cb8742149b213bb753363be72f2ed
Михаил Ермаков <mixer-ermakov@yandex.ru>
2023-04-05 21:00:52 +1000

    sprint_2_tema 5_7

--- main.py
+++ main.py
@@ -1,54 +1,51 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
-def attack(char_name, char_class):
-    """
-    Функция attack принимает два аргумента: имя персонажа и класс персонажа,
-    и возвращает строку с сообщением об атаке и сгенерированным уроном.
-    :param char_name: имя персонажа
-    :param char_class: класс персонажа
-    :return: возвращение текущего урона персонажу
+def attack(char_name: str, char_class: str) -> str:
+    """Функция, которая генерирует и возвращает строку с сообщением об атаке.
+    :param char_name: str, имя персонажа.
+    :param char_class: str, класс персонажа (warrior, mage или healer).
+    :return: str, строка с сообщением об атаке и сгенерированным уроном.
     """
     if char_class == 'warrior':
-        return (f"{char_name} нанёс урон противнику равный "
-                f"{5 + randint(3, 5)}")
+        return (f'{char_name} нанёс урон противнику равный '
+                f'{5 + randint(3, 5)}')
     if char_class == 'mage':
-        return (f"{char_name} нанёс урон противнику равный "
-                f"{5 + randint(5, 10)}")
+        return (f'{char_name} нанёс урон противнику равный '
+                f'{5 + randint(5, 10)}')
     if char_class == 'healer':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(-3, -1)}')
     return (f'{char_name} не Атаковал')
 
 
-def defence(char_name, char_class):
-    """
-    Функция которая  возвращает строку с сообщением о блокировании урона
-    и сгенерированными очками защиты
-    :param char_name: имя персонажа
-    :param char_class: класс персонажа
-    :return: возвращение текущего значения блокирования урона персонажу
+def defence(char_name: str, char_class: str) -> str:
+    """Функция, которая генерирует и возвращает строку с сообщением о блокировании.
+    :param char_name: str, имя персонажа.
+    :param char_class: str, класс персонажа (warrior, mage или healer).
+    :return: str, строка с сообщением о блокировании урона и сгенерированными
+    очками защиты.
     """
     if char_class == 'warrior':
-        return (f"{char_name} блокировал {10 + randint(5, 10)} ед. урона")
+        return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
-        return (f"{char_name} блокировал {10 + randint(-2, 2)} ед. урона")
+        return (f'{char_name} блокировал {10 + randint(-2, 2)} ед. урона')
     if char_class == 'healer':
-        return (f"{char_name} блокировал {10 + randint(2, 5)} ед. урона")
-    return (f"{char_name} не блокировал Атаку")
+        return (f'{char_name} блокировал {10 + randint(2, 5)} ед. урона')
+    return (f'{char_name} не блокировал Атаку')
 
 
-def special(char_name, char_class):
-    """
-    Функция которая возвращает строку с сообщением о применении
+def special(char_name: str, char_class: str) -> str:
+    """Функция которая возвращает строку с сообщением о применении.
     специального умения и сгенерированными очками для этого умения.
     :param char_name: имя персонажа
     :param char_class: класс персонажа
     :return: возвращение сгенерированных очков для этого умения
     """
     if char_class == 'warrior':
-        return (f"{char_name} применил специальное умение "
-                f"«Выносливость {80 + 25}»")
+        return (f'{char_name} применил специальное умение '
+                f'«Выносливость {80 + 25}»')
     if char_class == 'mage':
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
@@ -56,9 +53,8 @@ def special(char_name, char_class):
     return (f'{char_name} не применил специальное умение')
 
 
-def start_training(char_name, char_class):
-    """
-    Функция которая о запускает режим тренировки персонажа.
+def start_training(char_name: str, char_class: str) -> str:
+    """Функция которая о запускает режим тренировки персонажа.
     В зависимости от класса персонажа выводится соответствующее приветствие.
     :param char_name: имя персонажа
     :param char_class: класс персонажа
@@ -67,32 +63,31 @@ def start_training(char_name, char_class):
     :return: производит выход из функции тренировка
     """
     if char_class == 'warrior':
-        print(f"{char_name}, ты Воитель — отличный боец ближнего боя.")
+        print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
-        print(f"{char_name}, ты Маг — превосходный укротитель стихий.")
+        print(f'{char_name}, ты Маг — превосходный укротитель стихий.')
     if char_class == 'healer':
-        print(f"{char_name}, ты Лекарь — чародей, способный исцелять раны.")
-    print("Потренируйся управлять своими навыками.")
-    print("Введи одну из команд: attack — чтобы атаковать противника, "
-          "defence — чтобы блокировать атаку противника или "
-          "special — чтобы использовать свою суперсилу.")
-    print("Если не хочешь тренироваться, введи команду skip.")
+        print(f'{char_name}, ты Лекарь — чародей, способный исцелять раны.')
+    print('Потренируйся управлять своими навыками.')
+    print('Введи одну из команд: attack — чтобы атаковать противника, '
+          'defence — чтобы блокировать атаку противника или '
+          'special — чтобы использовать свою суперсилу.')
+    print('Если не хочешь тренироваться, введи команду skip.')
     cmd = None
     while cmd != 'skip':
-        cmd = input("Введи команду: ")
+        cmd = input('Введи команду: ')
         if cmd == 'attack':
             print(attack(char_name, char_class))
         if cmd == 'defence':
             print(defence(char_name, char_class))
         if cmd == 'special':
             print(special(char_name, char_class))
-    return "Тренировка окончена."
+    return 'Тренировка окончена.'
 
 
-def choice_char_class():
-    """
-    Функция которая запоминает выбор класса персонажа пользователем
-    и возращает выбранный класс персонажа
+def choice_char_class() -> str:
+    """Функция которая запоминает выбор класса персонажа пользователем.
+    и возращает выбранный класс персонажа.
     :param char_name: Ввод имени пользователя
     :param char_class: Выбор класса персонажа
     :param: approve_choice: подтверждение выбора персонажа
@@ -119,7 +114,13 @@ def choice_char_class():
     return char_class
 
 
-def main():
+
+main()
+
+
+# ...запишите:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
@@ -127,5 +128,5 @@ def main():
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
